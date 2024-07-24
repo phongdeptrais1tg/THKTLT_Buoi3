87a975c61649702c90b59453d757419937177cdd
phongdeptrais1tg <hoanganhphong2002hcm@gmail.com>
2024-07-24 13:42:34 +0700

    Buoi3_Bai1e

--- THKTLT_Buoi3/THKTLT_Buoi3/Bai1.cpp
+++ THKTLT_Buoi3/THKTLT_Buoi3/Bai1.cpp
@@ -53,6 +53,21 @@ void xuatBien(int a[MAX][MAX], int m, int n) {
     for (int j = 0; j < n; j++) printf("%4d", a[m - 1][j]);
     printf("\n");
 }
+void xuatCucDai(int a[MAX][MAX], int m, int n) {
+    printf("Phan tu cuc dai:\n");
+    for (int i = 1; i < m - 1; i++) {
+        for (int j = 1; j < n - 1; j++) {
+            if (a[i][j] > a[i - 1][j] && a[i][j] > a[i + 1][j] &&
+                a[i][j] > a[i][j - 1] && a[i][j] > a[i][j + 1]) {
+                printf("%4d", a[i][j]);
+            }
+            else {
+                printf("    ");
+            }
+        }
+        printf("\n");
+    }
+}
 int main() {
     srand(time_t(NULL));
     int a[MAX][MAX];
@@ -76,6 +91,7 @@ int main() {
         printf("2. Tinh va xuat tong gia tri tung dong\n");
         printf("3. Xuat phan tu lon nhat tren tung cot\n");
         printf("4. Xuat cac phan tu thuoc cac duong bien\n");
+        printf("5. Xuat cac phan tu cuc dai\n");
         printf("0. Thoat\n");
         printf("Moi ban chon: ");
         scanf_s("%d", &chon);
@@ -91,6 +107,9 @@ int main() {
             break;
         case 4:
             xuatBien(a, m, n);
+            break;
+        case 5:
+            xuatCucDai(a, m, n);
             break;
         case 0:
             printf("Thoat chuong trinh\n");
